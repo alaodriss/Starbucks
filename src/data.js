8e279a0a07980b00eea6657e4911b47b73366fb2
alaodriss <alaodriss@gmail.com>
2023-08-20 14:38:37 +0200

    add style

--- src/data.js
+++ src/data.js
@@ -2,22 +2,22 @@ import icon1 from './assets/images/getting-started-1_2x.jpg'
 import icon2 from './assets/images/getting-started-2_2x.jpg'
 import icon3 from './assets/images/getting-started-3_2x.jpg'
 
-export const Getting = [
+export  const Gettings = [
     {
         id:1,
-        icon:{icon1},
+        icon: icon1,
         title: 'Create an account',
         text:'To get started, join now. You can also join in the app to get access to the full range of Starbucks® Rewards benefits.'
     },
     {
         id:2,
-        icon:{icon2},
+        icon:icon2,
         title: 'Order and pay how you’d like',
         text:'Use cash, credit/debit card or save some time and pay right through the app. You’ll collect Stars all ways. Learn how'
     },
     {
         id:3,
-        icon:{icon3},
+        icon:icon3,
         title: 'Earn Stars, get Rewards',
         text:'As you earn Stars, you can redeem them for Rewards—like free food, drinks, and more. Start redeeming with as little as 25 Stars!'
     }
--- src/pages/Rewads.js
+++ src/pages/Rewads.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import back from '../assets/images/xl-hero.jpg'
+import {Gettings} from '../data'
 import './Reawds.css'
 
 const Rewads = () => {
   return (
+    <> 
     <div className='slider'>
       <div className='slider-content'>
         <h1>FREE COFFEEIS <br/>A TAP AWAY</h1>
@@ -15,6 +17,27 @@ const Rewads = () => {
           <img src={back} alt='backgournd' />
       </div>
     </div>
+    <div className='getting-container'>
+         <h2>Getting started is easy</h2>
+         <p>Earn Stars and get rewarded in a few easy steps.</p>
+         <div className='gettieg-content' >
+      {Gettings.map((item)=> {
+        const {id,icon,title,text}=item
+        return (
+          
+            <div className='content' key={id}>
+            <img src={icon} alt={title} />
+            <h3>{title}</h3>
+            <p>{text}</p>
+            </div>
+        )
+      })
+    }
+      </div>
+     
+
+    </div>
+    </>
   )
 }
 
